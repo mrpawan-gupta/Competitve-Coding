b55b752c48645cbe71a1964bc09f409ab170fae6
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-15 19:54:11 +0530

    Solution : 231A

---
+++ 01. CodeForces/1]. Problem Set/A_Team.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, Petya, Vasya, Tonya, number(0);
+    cin >> n;
+    while (n--)
+    {
+        cin >> Petya >> Vasya >> Tonya;
+        if (Petya + Vasya + Tonya >= 2)
+        {
+            number += 1;
+        }
+    }
+    cout << number << endl;
+    return 0;
+}
