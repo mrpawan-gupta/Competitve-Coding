cca82d03a9700e0b7313c23b8c53752cad2272fa
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-16 13:28:28 +0530

    SOlution : 66. Plus One

---
+++ 03. LeetCode/1]. Problems/C++/0066)_Plus_One.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+	int n = digits.size();
+	for (int i = n - 1; i >= 0; --i)
+	{
+		if (digits[i] == 9)
+		{
+			digits[i] = 0;
+		}
+		else
+		{
+			digits[i]++;
+			return digits ;
+		}
+	}
+		digits[0] =1;
+		digits.push_back(0);
+    return digits ;
+    }
+};
