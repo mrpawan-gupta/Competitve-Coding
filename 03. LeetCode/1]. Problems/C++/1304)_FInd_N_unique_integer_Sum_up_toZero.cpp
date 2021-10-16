86abf843660c32e9808c5ab7fa43a982937b5542
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-16 13:36:02 +0530

    Solution : 1304. Find N Unique Integers Sum up to Zero

---
+++ 03. LeetCode/1]. Problems/C++/1304)_FInd_N_unique_integer_Sum_up_toZero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        
+        vector<int> A(n);
+        
+        for (int i = 0; i < n; ++i) A[i] = i * 2 - n + 1;
+        return A;
+       
+    }
+};
