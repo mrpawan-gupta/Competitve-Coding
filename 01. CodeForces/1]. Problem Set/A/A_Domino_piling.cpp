1b36f91c6781d897b93faac7e2c3d69dc2e036cb
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-28 01:38:42 +0530

    Create 50 A - Domino piling.cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Domino_piling.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int m, n;
+    cin >> m >> n;
+
+    int box = m * n;
+
+    cout << box / 2;
+    // no of boxes made of size 2*1 
+    
+    return 0;
+}
