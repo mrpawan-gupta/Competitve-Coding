231ff70a6071ed4ad015f83e93719fcc6cef30e1
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-28 22:40:59 +0530

    Create 110 A - Nearly Lucky Number.cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    long long n;
+    cin >> n;
+    int count = 0;
+    while (n != 0)
+    {
+        if (n % 10 == 4 || n % 10 == 7)
+        {
+            count += 1;
+        }
+        n /= 10;
+    }
+    if (count == 4 || count == 7)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
