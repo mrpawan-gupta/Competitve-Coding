d34ed73b6472cc65f299299f7770c7b34fb687b1
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-01 14:22:39 +0530

    Solution : HLPNISHANT

--- 02. CodeChef/2]. Compete/5].Others/SnackDown2021/Help_Nishant.cpp
+++ 02. CodeChef/2]. Compete/5].Others/SnackDown2021/Help_Nishant.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long ;
+using ll = long long int;
 using vb = vector<bool>;
 using vvb = vector<vb>;
 using vi = vector<int>;
@@ -12,31 +12,34 @@ using vvc = vector<vc>;
 using vs = vector<string>;
 const ll mod = 1e9 + 7, inf = 1e18;
 #define pb push_back
-#define fast   ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-vector<ll> v(1e6+1,0);
-void ValueCheck(){
-    v[1]=1;
-    v[2]=2;
-    for(ll i=3;i<=1e6;i++){
-        v[i]=v[i-1]+i-1;
-    }
-}
-void solve(){
-    ll k;
-    cin>>k;
-    ll pos=lower_bound(v.begin(),v.end(),k+1)-v.begin()-1;
-    cout<<2*pos<<endl;
-}
+int main(){
+    fast;
 
-int main()
-{
-    ll t=1;
-    ValueCheck();
-    cin>>t;
+    int t;
+    cin >> t;
+
     while(t--){
-        solve();
+        ll k;
+        cin >> k;
+
+        ll s = 1;
+        ll e = 10e9;
+        int result = 0;
+        while (s <= e)
+        {
+            ll mid = (s + e) / 2;
+            if(mid*(mid+1)/2+1 <=k){
+                result = mid + 1;
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
+        }
+        cout << 2 * result << '\n';
     }
-    
+
     return 0;
 }
