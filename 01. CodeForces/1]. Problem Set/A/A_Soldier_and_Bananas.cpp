fa7d6c9ca354d016235a640a3d42abd0e36e910d
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-03 15:29:20 +0530

    Create 546A - Soldier and Bananas.cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Soldier_and_Bananas.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    ll k, n, w;
+    cin >> k >> n >> w;
+
+    ll sum = (w * ((w + 1)) * k)/2;
+    
+    if (sum > n){
+        cout << sum - n;
+    }
+    else{
+        cout << 0;
+    }
+    return 0;
+}
