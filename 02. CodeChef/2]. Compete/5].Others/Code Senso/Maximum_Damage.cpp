bf0bc0e726376ff5c76017c1b38cdec17363c868
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-04 12:28:12 +0530

    SOlution : MAXDMGE

---
+++ 02. CodeChef/2]. Compete/5].Others/Code Senso/Maximum_Damage.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+int main()
+{
+    fast;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        vector<ll> v(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            ll res = INT_FAST64_MIN;
+            if (i - 1 >= 0)
+            {
+                res = max(res, (v[i] & v[i - 1]));
+            }
+            if (i + 1 < n)
+            {
+                res = max(res, (v[i] & v[i + 1]));
+            }
+            cout << res << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
