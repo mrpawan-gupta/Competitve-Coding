492785e7231ab6e2339f32a3fbf47c94a35bfe20
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-05 20:08:05 +0530

    Solution Problem Code:DIAGMOVE

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/Diagonal_movement.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+int main()
+{
+    fast;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if ((x + y) % 2 == 0)
+        {
+            cout << "YES" << '\n';
+        }
+        else
+        {
+            cout << "NO" << '\n';
+        }
+    }
+
+    return 0;
+}
