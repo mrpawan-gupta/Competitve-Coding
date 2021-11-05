b89dfe65f1cbfd8c12d547eb38c2145fab841682
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-06 00:29:32 +0530

    Solution : Problem Code:EQUALCOIN

---
+++ 02. CodeChef/2]. Compete/1]. Long Challenge/Long Challenge November 2021/Equal_Coins.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+int main()
+{
+    fast;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+
+        if (x == 0 && y % 2 == 0)
+        {
+            cout << "YES" << endl;
+            continue;
+        }
+        else if (x == 0 && y % 2 != 0)
+        {
+            cout << "NO" << endl;
+            continue;
+        }
+
+        if ((x + (y * 2)) % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
