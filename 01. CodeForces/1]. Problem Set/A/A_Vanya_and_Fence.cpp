4014b0ef3e37c65ca2dbbf4da3ad459969fe5657
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-07 17:28:22 +0530

    Create 677A - Vanya and Fence.Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Vanya_and_Fence.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+int main()
+{
+    fast;
+
+    int h, n;
+    cin >> n >> h;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int c = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > h)
+        {
+            c += 2;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    cout << c << endl;
+
+    return 0;
+}
