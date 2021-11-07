779ddabcb305eab2779686f378d44b93f16b97b2
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-07 23:44:43 +0530

    Solution : Problem Code : HS08TEST

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/ATM.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int a;
+    float b;
+    cin >> a >> b;
+    if (a % 5 == 0 && a <= b - 0.5)
+        b = b - a - 0.50;
+    cout << fixed << setprecision(2) << b;
+
+    return 0;
+}
