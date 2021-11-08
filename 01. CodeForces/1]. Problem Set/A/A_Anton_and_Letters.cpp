8708c055eb59ace3582fc12723da15382296d9d2
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-08 15:20:03 +0530

    Create 443A - Anton and Letters.Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Anton_and_Letters.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    string s;
+    set<char> a;
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+            a.insert(s[i]);
+    }
+    cout << a.size() << endl;
+    return 0;
+
+    return 0;
+}
