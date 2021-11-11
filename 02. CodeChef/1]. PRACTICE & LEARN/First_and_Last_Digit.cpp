07397de10ce38fa60619b56fd731e6864f6cedc3
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-11 20:14:19 +0530

    Solution Problem Code : FLOW004

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/First_and_Last_Digit.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        int last = n % 10;
+
+        int digits = log10(n);
+        int first = n / pow(10, digits);
+        
+        cout << first + last << endl;
+        
+    }
+
+    return 0;
+}
