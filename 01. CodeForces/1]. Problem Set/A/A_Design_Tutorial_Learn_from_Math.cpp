b1b2b88b526f99945b7967ecb21be9d9b5f0260e
Pawan Gupta <guptapawanro2017@gmail.com>
2021-11-16 20:59:20 +0530

    Create 472A - Design Tutorial: Learn from Math.Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << n - 4 << " 4";
+    }
+    else
+    {
+        cout << n - 9 << " 9";
+    }
+
+    return 0;
+}
