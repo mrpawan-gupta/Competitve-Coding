79c8549350cb0895529ca629e7313944aeb3602a
mrpawan-gupta <guptapawanro2017@gmail.com>
2021-11-19 19:55:13 +0530

    Solution Problem Code : CFLK2021

---
+++ 02. CodeChef/2]. Compete/5].Others/Code SMASH!/Count_Votes.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+int main()
+{
+    fast;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        vi a(3);
+        cin >> a[0] >> a[1] >> a[2];
+
+        int x = *max_element(a.begin(), a.end());
+
+        int c = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (a[i] == x)
+            {
+                c++;
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (a[i] == x && c == 1)
+            {
+                cout << 0 << " ";
+            }
+            else if (a[i] == x && c!=1)
+            {
+                cout << 1 << " ";
+            }
+            else
+            {
+                cout << (x - a[i]) + 1 << " ";
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
