db8b0bc0c900aaa5f97f31138b82bf6846d154d6
mrpawan-gupta <guptapawanro2017@gmail.com>
2021-11-21 14:47:48 +0530

    Solution Problem: CONFLIP Contest: LRNDSA01

---
+++ 02. CodeChef/2]. Compete/5].Others/DSA Learning Series/Complexity Analysis + Basics Warm Up/Coin_Flip.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+using l = long;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+int main()
+{
+    fast;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int g;
+        cin >> g;
+        int i, n, q;
+
+        while (g--)
+        {
+            cin >> i >> n >> q;
+            if (n % 2 == 0)
+            {
+                cout << n / 2 << endl;
+            }
+            else
+            {
+                if (i == q)
+                {
+                    cout << n / 2 << endl;
+                }
+                else
+                {
+                    cout << (n / 2) + 1 << endl;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
