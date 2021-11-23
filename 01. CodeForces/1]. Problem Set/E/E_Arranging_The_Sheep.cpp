1dc6097a18c597f51ab0ef83ad359cbedd528f5b
mrpawan-gupta <guptapawanro2017@gmail.com>
2021-11-23 23:50:27 +0530

    Create E - Arranging The Sheep.Cpp

---
+++ 01. CodeForces/1]. Problem Set/E/E_Arranging_The_Sheep.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int c = 0;
+        for (auto x : s){
+            c += (x == '*' ? 1 : 0);
+        }
+        int pos = -1;
+        int cur = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '*')
+            {
+                cur++;
+                if (cur == c / 2){
+                    pos = i;
+                }
+            }
+        }
+        ll ans = 0;
+        cur = pos - c / 2;
+        for (int i = 0; i < n; i++){
+            if (s[i] == '*')
+            {
+                ans += abs(cur - i);
+                cur++;
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
