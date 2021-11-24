9599d96774f6028e248c3dba54bdd7a5dbe5de19
mrpawan-gupta <guptapawanro2017@gmail.com>
2021-11-24 20:51:26 +0530

    Solution Hardest Problem Bet Problem Code: HARDBET

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/Hardest_Problem_Bet.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a <= b && a <= c)
+            cout << "Draw" << endl;
+
+        else if (b <= a && b <= c)
+            cout << "Bob" << endl;
+
+        else
+            cout << "Alice" << endl;
+    }
+
+    return 0;
+}
