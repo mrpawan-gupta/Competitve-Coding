94cadad90d9ebd27819ae3d8aadcf9aa24343816
mrpawan-gupta <guptapawanro2017@gmail.com>
2021-11-24 18:48:15 +0530

    Create 133A - HQ9+ .Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_HQ9_.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    string s;
+    cin >> s;
+    int len = s.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == 'H' || s[i] == 'Q' || s[i] == '9')
+        {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+
+    return 0;
+}
