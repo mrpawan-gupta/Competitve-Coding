4cf15cd6f1832e8e687a85010c56f30e73546016
mrpawan-gupta <guptapawanro2017@gmail.com>
2021-11-29 19:43:37 +0530

    Create 337A - Puzzles.Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Puzzles.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int n, m, f[1000];
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> f[i];
+    }
+    sort(f, f + m);
+    int least = f[n - 1] - f[0];
+    for (int i = 1; i <= m - n; ++i)
+    {
+        if (f[i + n - 1] - f[i] < least)
+        {
+            least = f[i + n - 1] - f[i];
+        }
+    }
+    cout << least << endl;
+
+    return 0;
+}
