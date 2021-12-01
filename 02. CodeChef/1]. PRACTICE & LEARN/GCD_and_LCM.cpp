3fed5630fe3655ea297769b47b92a3c5cbc6e0fc
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-01 19:18:20 +0530

    Solution GCD and LCM Added

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/GCD_and_LCM.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+    
+    while(t-->0){
+        ll a, b;
+        cin >> a >> b;
+
+        ll x = __gcd(a, b);
+
+        cout << x << " " << (a * b) / x << endl;
+    }
+
+    return 0;
+}
