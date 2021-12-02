dbcb6498928708f7a031ea1662d90c28db42ea88
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-02 21:13:31 +0530

    Solution Two Vs Ten

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/Two_vs_Ten.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        ll x;
+        cin >> x;
+
+        if(x%10==0){
+            cout << 0 << endl;
+        }
+        else if(x%10==5){
+            cout << 1 << endl;
+        }
+        else{
+            cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
