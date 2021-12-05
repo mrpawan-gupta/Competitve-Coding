777980a59e3ca6d364bac195a2e5aff61eb12068
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-05 17:42:19 +0530

    Create 1433A - Boring Apartments .Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Boring_Apartments.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string x;
+        cin >> x;
+        int dig = x[0] - '0' - 1;
+        int len = x.size();
+        cout << dig * 10 + len * (len + 1) / 2 << endl;
+    }
+
+    return 0;
+}
