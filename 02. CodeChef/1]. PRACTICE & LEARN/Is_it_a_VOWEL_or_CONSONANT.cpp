a041aabe8dd2b77ccbcf6338758efee23556c9ea
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-06 20:27:05 +0530

    Solution Is it a VOWEL or CONSONANT Added

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/Is_it_a_VOWEL_or_CONSONANT.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    char c;
+    bool isLowercaseVowel, isUppercaseVowel;
+    cin >> c;
+
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    
+    if(isLowercaseVowel || isUppercaseVowel)
+        cout << "Vowel" << endl;
+    else
+        cout << "Consonant" << endl;
+
+    return 0;
+}
