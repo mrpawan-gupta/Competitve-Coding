b0be599791312a82650f114ea90ac4933c4a5672
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-07 00:16:04 +0530

    Solution Area OR Perimeter Added

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/Area_OR_Perimeter.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int l, b;
+    cin >> l >> b;
+
+    int a = l * b;
+    int p = 2 * (l + b);
+
+    if(a>p){
+        cout << "Area" << endl;
+        cout << a << endl;
+    }
+    else if(a<p){
+        cout << "Peri" << endl;
+        cout << p << endl;
+    }
+    else if (a==b){
+        cout << "Eq" << endl;
+    }
+
+    return 0;
+}
