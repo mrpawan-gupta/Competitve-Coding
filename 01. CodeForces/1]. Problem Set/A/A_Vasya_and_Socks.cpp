9d39c4fa9e6f6f6f9032e901a9a6e52645654a91
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-08 20:06:40 +0530

    Create 460A - Vasya and Socks .Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Vasya_and_Socks.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int n, m;
+    cin >> n >> m;
+
+    int c = n;
+
+    while (n >= m){
+        c += n / m;
+        n = n / m + n % m;
+    }
+
+    cout << c << '\n';
+
+    return 0;
+}
