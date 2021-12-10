c3d8099ffad19281033c054a97018330fe1453c8
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-10 14:13:10 +0530

    Create  	313A - Ilya and Bank Account .Cpp

---
+++ 02. CodeChef/1]. PRACTICE & LEARN/A_Ilya_and_Bank_Account.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    long n;
+    cin >> n;
+    if (n >= 0){
+        cout << n << endl;
+    }
+    else{
+        int a = n / 10;
+        int b = n / 100 * 10 + n % 10;
+        cout << max(a, b) << endl;
+    }
+
+    return 0;
+}
