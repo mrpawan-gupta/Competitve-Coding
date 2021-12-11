084070b3daadc4889f3582debdfb92a5d56e66ad
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-11 10:33:35 +0530

    Create 1399B - Gifts Fixing.Cpp

---
+++ 01. CodeForces/1]. Problem Set/B/B_Gifts_Fixing.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (auto &item : a){
+        cin >> item;
+    }
+    for (auto &item : b){
+        cin >> item;
+    }
+    int MinA = *min_element(a.begin(), a.end());
+    int MinB = *min_element(b.begin(), b.end());
+    ll ans = 0;
+    for (int i = 0; i < n; ++i){
+        ans += max(a[i] - MinA, b[i] - MinB);
+    }
+    cout << ans << endl;
+}
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
