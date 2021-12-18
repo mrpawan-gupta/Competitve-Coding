08ca55e7b81745c366ed1c86dcd528c9c03af100
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-18 23:14:15 +0530

    Solution Eductional CF Round 119 A - Equal or Not Equal

---
+++ 01. CodeForces/2]. Contests/Educational Codeforces Round 119/A_Equal_or_Not_Equal.cpp
@@ -0,0 +1,62 @@
+/*
+
+author  ---- > Pawan Gupta
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    fast;
+
+    int t;
+    cin >> t;
+
+    // testcases
+
+    while(t--){
+        string string;
+        cin >> string;
+        
+        ll cnt = 0;
+        for (auto i = 0; i < string.size(); i++)
+        {
+            if (string[i] == 'N'){
+                cnt++;
+            }
+        }
+
+        // print answer
+        if (cnt == 1){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+
+    return 0;
+}
