985c0e1730624cc7b5b4232d36b8b3a4c9ee3a52
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-22 13:19:35 +0530

    Create 749A - Bachgold Problem.Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Bachgold_Problem.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using mii = map<int, int>;
+using mll = map<long long, long long>;
+using umii = unordered_map<int, int>;
+using umll = unordered_map<long long, long long>;
+const ll mod = 1e9 + 7, inf = 1e18;
+#define loop(a, b, c) for (auto a = b; a < c; a++)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+int main()
+{
+    fast;
+
+    int n;
+    cin >> n;
+    if (n % 2 == 1)
+    {
+        n /= 2;
+        n--;
+        cout << n + 1 << endl;
+        cout << 3 << " ";
+    }
+    else
+    {
+        n /= 2;
+        cout << n << endl;
+    }
+
+    loop(i, 0, n)
+    {
+        cout << 2 << " ";
+    }
+
+    return 0;
+}
