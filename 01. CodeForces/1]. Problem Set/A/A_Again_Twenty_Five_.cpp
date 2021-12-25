661783bab84e21677339af5568f22ba3ba18ef26
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-25 10:08:58 +0530

    Create 630A - Again Twenty Five!.Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Again_Twenty_Five_.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvl = vector<vl>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using mii = map<int, int>;
+using mll = map<long long, long long>;
+using umii = unordered_map<int, int>;
+using umll = unordered_map<long long, long long>;
+const long long mod = 1e9 + 7, inf = 1e18;
+#define loop(a, b, c) for (auto a = b; a < c; a++)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define F first
+#define endl "\n"
+#define S second
+#define all(x) x.begin(), x.end()
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void solveTestCases(){
+
+}
+int main(){
+    fast;
+
+    long long int n;
+    cin >> n;
+
+    cout << 25 << endl;
+
+    return 0;
+}
