ff96b059467194d2f4afe7e54084a2428b11e209
Pawan Gupta <guptapawanro2017@gmail.com>
2022-02-23 22:58:23 +0530

    Solution Shopping Change Added

---
+++ 02. CodeChef/2]. Compete/4]. Starters/2022 CodeChef Starters 27/Shopping_Change.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(){
+    int n;
+    cin >> n;
+    cout << 100 - n << endl;
+    return 0;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
