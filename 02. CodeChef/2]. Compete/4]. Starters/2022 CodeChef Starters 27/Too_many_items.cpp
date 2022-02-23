c93f2352dcbf069b2a23a324faab38a6976b7673
Pawan Gupta <guptapawanro2017@gmail.com>
2022-02-23 22:59:12 +0530

    Solution Too many items Added

---
+++ 02. CodeChef/2]. Compete/4]. Starters/2022 CodeChef Starters 27/Too_many_items.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(){
+    
+    double n;
+    cin >> n;
+
+    cout << ceil(n / 10) << endl;
+
+    return 0;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
