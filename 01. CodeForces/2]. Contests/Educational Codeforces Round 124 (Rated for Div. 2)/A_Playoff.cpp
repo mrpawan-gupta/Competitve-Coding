06c993b9a243cb05a3badc3d84cd17b6676a61b2
Pawan Gupta <guptapawanro2017@gmail.com>
2022-03-10 22:08:08 +0530

    Solution Eductional CF Round 124 A - Playoff

---
+++ 01. CodeForces/2]. Contests/Educational Codeforces Round 124 (Rated for Div. 2)/A_Playoff.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solveTestCase(){
+            int n;
+            cin >> n;
+            cout << (1ll << n) - 1 << endl;
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        Solution ClassObject;
+        ClassObject.solveTestCase();
+    }
+
+    return 0;
+}
