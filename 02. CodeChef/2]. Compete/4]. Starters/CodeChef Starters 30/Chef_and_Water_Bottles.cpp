2f6cfb0c013ef6d4755fc38dbef478dfec0e1f6e
Pawan Gupta <guptapawanro2017@gmail.com>
2022-03-18 22:53:30 +0530

    Solution Problem Code:CHEFBOTTLE

---
+++ 02. CodeChef/2]. Compete/4]. Starters/CodeChef Starters 30/Chef_and_Water_Bottles.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solveTestCase(){
+            int n, x;
+            int k;
+
+            cin >> n >> x >> k;
+
+            long cnt = k / x;
+
+            if(cnt>=n){
+                cout << n << endl;
+                return;
+            }
+
+            cout << cnt << endl;
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        Solution ClassObject;
+        ClassObject.solveTestCase();
+    }
+
+    return 0;
+}
