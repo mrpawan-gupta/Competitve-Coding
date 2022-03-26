feafa16987e8a6793bc88403a388321f0e9b1e05
Pawan Gupta <guptapawanro2017@gmail.com>
2022-03-26 12:31:35 +0530

    Solution 704. Binary Search Added

---
+++ 03. LeetCode/1]. Problems/C++/0704)_Binary Search.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
