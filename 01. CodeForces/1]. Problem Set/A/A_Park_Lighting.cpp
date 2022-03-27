e9f2169a31cf3d8415794d5ad32b4ebf568c1d53
Pawan Gupta <guptapawanro2017@gmail.com>
2022-03-27 19:01:40 +0530

    Create 1358A - Park Lighting.Cpp

---
+++ 01. CodeForces/1]. Problem Set/A/A_Park_Lighting.cpp
@@ -0,0 +1,14 @@
+/*
+    Solution By :- Pawan Gupta [ CF - Mr.pawan_gupta, CC - mrpavan_gupta ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t, n, m;
+    cin >> t;
+    while (t--){
+        cin >> n >> m;
+        cout << (n * m + 1) / 2 << endl;
+    }
+}
