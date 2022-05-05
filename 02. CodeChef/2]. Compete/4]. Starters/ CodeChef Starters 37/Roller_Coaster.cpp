110ab315c40c2a5b3450811830622a170dd652cb
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-05 11:04:37 +0530

    Solution Starter 37 Roller Coaster

---
+++ 02. CodeChef/2]. Compete/4]. Starters/ CodeChef Starters 37/Roller_Coaster.cpp
@@ -0,0 +1,31 @@
+/*
+    Solution By :- Pawan Gupta [ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int n, m;
+          cin >> n >> m;
+          (n>=m)?cout << "YES":cout << "NO";
+          cout << '\n';
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
