4a0059b974eca6584f742843ff606d022feb292b
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-18 22:47:29 +0530

    Solution CodeChef Starters 39 Chess Ratings

---
+++ 02. CodeChef/2]. Compete/4]. Starters/CodeChef Starters 39/Chess_Ratings.cpp
@@ -0,0 +1,31 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int x,y;
+          cin>>x>>y;
+          int cnt = y-x;
+          cout<< (cnt+7) / 8 << endl;
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
