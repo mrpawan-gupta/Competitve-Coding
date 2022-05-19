5fb79b01cc6a3e22347c4a9c5e069260be94fa65
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-19 13:14:34 +0530

    Solution 329. Longest Increasing Path in a Matrix Added

--- 03. LeetCode/1]. Problems/C++/0329)_Longest Increasing Path in a Matrix.cpp
+++ 03. LeetCode/1]. Problems/C++/0329)_Longest Increasing Path in a Matrix.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int help(vector<vector<int>>& mat, int i, int j, vector<vector<int>>& h) {
-        if(h[i][j] != -1) return h[i][j];
-        int dx[] = {0,0,1,-1};
-        int dy[] = {1,-1,0,0};
-        int count = 0;
-        for(int n = 0; n < 4; n++){
-            int new_i = dx[n] + i;
-            int new_j = dy[n] + j;
-            if(new_i >= 0 && new_j >= 0 && new_i < mat.size() && new_j < matridx[i].size()) {
-                if(matridx[new_i][new_j] > matridx[i][j])
-                count = max(count,1+help(mat,new_i,new_j,h));
-            }
-        }
-        h[i][j] = count;
-        return h[i][j];
+    int m, n;
+    int dp(int x, int y, vector<vector<int>>& depth, vector<vector<int>>& matrix ) {
+        if(depth[x][y] != -1) return depth[x][y];
+        int mx =1;
+        if(x<m-1 && matrix[x][y]<matrix[x+1][y])
+            mx = max(mx,1 + dp(x+1,y,depth,matrix));
+        if(x>0 && matrix[x][y]<matrix[x-1][y])
+            mx = max(mx,1 + dp(x-1,y,depth,matrix));
+        if(y<n-1 && matrix[x][y]<matrix[x][y+1])
+            mx = max(mx,1 + dp(x,y+1,depth,matrix));
+        if(y>0 && matrix[x][y]<matrix[x][y-1])
+            mx = max(mx,1 + dp(x,y-1,depth,matrix));
+        depth[x][y] = mx;
+        return mx;
     }
-    
-    int longestIncreasingPath(vector<vector<int>>& mat) {
-        vector<vector<int>> h (mat.size(),vector<int>(matridx[0].size(),-1));
-        int ans = 0;
-        for(int i = 0; i < mat.size(); i++)
-        {
-            for(int j = 0; j < matridx[0].size(); j++)
-            {
-               ans = max(ans,1+help(mat,i,j,h));
-            }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int res = 1;     
+        m = matrix.size();
+        n = matrix[0].size();
+        vector<vector<int> > depth( m , vector<int> (n, -1));  
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++)
+            res = max(res,dp(i,j,depth,matrix));
         }
-        return ans;
+        return res;
     }
 };
