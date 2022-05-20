23d5107da4505a5fef0582ebb1003962c4148289
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-20 12:18:45 +0530

    DSA Learning Series - Triangle With Angle

---
+++ 02. CodeChef/2]. Compete/5].Others/DSA Learning Series/Getting started Practice Contest/Triangle_With_Angle.cpp
@@ -0,0 +1,32 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int a,b,c;
+          cin>>a>>b>>c;
+          if ((a+b+c==180) && a>0 && b>0 && c>0)  cout<<"YES";
+          else cout<<"NO";
+          
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
