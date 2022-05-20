25f38ac89957779c850da110e1ef7f5ab9b04371
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-20 12:16:35 +0530

    DSA Learning Series - Find Second Largest

---
+++ 02. CodeChef/2]. Compete/5].Others/DSA Learning Series/Getting started Practice Contest/Find_Second_Largest.cpp
@@ -0,0 +1,31 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int a[3];
+          cin >> a[0] >> a[1] >> a[2];
+          sort(a, a+3);
+          cout << a[1] << endl;
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
