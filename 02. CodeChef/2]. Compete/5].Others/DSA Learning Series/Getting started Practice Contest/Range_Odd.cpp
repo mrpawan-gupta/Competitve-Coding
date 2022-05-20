e785648938e9f77facb7bfc20d5bb1080284db7a
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-20 12:16:52 +0530

    DSA Learning Series - Range Odd

---
+++ 02. CodeChef/2]. Compete/5].Others/DSA Learning Series/Getting started Practice Contest/Range_Odd.cpp
@@ -0,0 +1,35 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int x,y;
+          cin >> x >> y;
+
+          for(int i = x; i <=y; i++){
+            if((i&1)){
+              cout << i << ' ';
+            }
+          }
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
