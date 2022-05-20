1829cdcf28ae3c6e92b6d026e97341e3c507678c
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-20 12:17:06 +0530

    DSA Learning Series - Raju and His Trip

---
+++ 02. CodeChef/2]. Compete/5].Others/DSA Learning Series/Getting started Practice Contest/Raju_and_His_Trip.cpp
@@ -0,0 +1,31 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int n; cin >> n;
+          if(n % 5 == 0 || n % 6 ==0)cout << "YES" << endl;
+          else cout << "NO" << endl;
+          
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
