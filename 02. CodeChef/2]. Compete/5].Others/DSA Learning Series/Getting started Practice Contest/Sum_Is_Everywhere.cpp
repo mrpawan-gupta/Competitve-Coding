8eccc72f19954da8613eebaa0e55545c2cb8f69b
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-20 12:18:31 +0530

    DSA Learning Series - Sum Is Everywhere

---
+++ 02. CodeChef/2]. Compete/5].Others/DSA Learning Series/Getting started Practice Contest/Sum_Is_Everywhere.cpp
@@ -0,0 +1,29 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          long int n; cin >> n;
+          cout << n * n << " " << n * ( n+ 1) ;
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
