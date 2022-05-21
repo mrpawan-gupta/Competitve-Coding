32940e135cec3953f7f1edbe2bf4542bda486cb7
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-21 17:17:42 +0530

    Solution May Long Total Prize Money

---
+++ 02. CodeChef/2]. Compete/1]. Long Challenge/May Long Two 2022/Total_Prize_Money.cpp
@@ -0,0 +1,31 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int a, b;
+          cin >> a >> b;
+
+          cout << ((a*10) + (90*b)) << '\n';
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
