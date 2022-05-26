33869a920b403ab9a16649a2f030a410f8cae111
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-26 23:21:28 +0530

    Solution Increasing Array

--- 04. CSES /ProblemSet/Increasing_Array.cpp
+++ 04. CSES /ProblemSet/Increasing_Array.cpp
@@ -7,7 +7,21 @@ using namespace std;
 class Solution{
     public:
         void solve(){
-
+            long long n, d = 0, count = 0;
+            cin>>n;
+            long long a[n];
+            for (int i = 0; i<n; i++)
+            cin>>a[i];
+            for (int i = 0; i<n-1; i++) {
+                d = 0;
+                if(a[i+1]<a[i])
+                {
+                    d = a[i] - a[i+1];
+                    a[i+1] = a[i];
+                    count = count + d;
+                }
+            }
+            cout<<count<<endl;
         }
 };
 
