6a8beaf63578befe4ca6c94b58c53f6d606d1028
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-02 20:28:22 +0530

    Solution Starters 41 Chef and Brain Speed

---
+++ 02. CodeChef/2]. Compete/4]. Starters/CodeChef Starters 41/Chef_and_Brain_Speed.cpp
@@ -0,0 +1,34 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+          int a, b; 
+          cin >> a >> b; 
+          if(a<b){
+            cout  << "YES" << endl;
+          }else{
+            cout << "NO" << endl;
+          }
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
