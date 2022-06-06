f3e77032b7bf2a3db12b2044764872f9f16d2cdb
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-06 16:32:33 +0530

    Soolution 66. Plus One Added

--- 03. LeetCode/1]. Problems/C++/0066)_Plus_One.cpp
+++ 03. LeetCode/1]. Problems/C++/0066)_Plus_One.cpp
@@ -1,21 +1,18 @@
-class Solution {
+class Solution{
 public:
-    vector<int> plusOne(vector<int>& digits) {
-	int n = digits.size();
-	for (int i = n - 1; i >= 0; --i)
-	{
-		if (digits[i] == 9)
-		{
-			digits[i] = 0;
+	vector<int> plusOne(vector<int> &digits){
+		int n = digits.size();
+		for (int i = n - 1; i >= 0; --i){
+			if (digits[i] == 9){
+				digits[i] = 0;
+			}
+			else{
+				digits[i]++;
+				return digits;
+			}
 		}
-		else
-		{
-			digits[i]++;
-			return digits ;
-		}
-	}
-		digits[0] =1;
+		digits[0] = 1;
 		digits.push_back(0);
-    return digits ;
-    }
+		return digits;
+	}
 };
