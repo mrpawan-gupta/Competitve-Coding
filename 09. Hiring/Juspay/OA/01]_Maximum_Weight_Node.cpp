0cfcc084a740d898a633b0cc48e1e63dc8965f94
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-10 11:09:01 +0530

    Juspay OA Max Weight Node

---
+++ 09. Hiring/Juspay/OA/01]_Maximum_Weight_Node.cpp
@@ -0,0 +1,48 @@
+/*
+Problem Description :- Maximum Weight Node
+Given a maze with N cells. Each cell may have multiple entry points but not more than one exit(i.e entry/exit points are unidirectional doors like valves).
+
+You are given an array Edge[] of N integers, where Edge[i] contains the cell number that can be reached from of cell i in one step. Edge[i] is -1 if the ith cell doesn’t have an exit.
+
+The task is to find :- the node number of maximum weight node(Weight of the node is the sum of node numbers of all nodes pointing to that node).
+
+Note:- The cells are named with an integer value from 0 to N-1. If there is no node pointing to the ith node then weight of the ith node is zero.
+
+INPUT FORMAT :-
+
+The first line contains the number of cells N.
+The second line has a list of N values of the edge[ ] array, where edge[i] conatins the cell number that can be reached from cell 'i' in one step. edge[i] is -1 if the ith doesn't have ans exit.
+OUTPUT FORMAT :
+
+First line denotes the node number with maximum weight node.
+Sample Input :
+23
+4 4 1 4 13 8 8 8 0 8 14 9 15 11 -1 10 15 22 22 22 22 22 21
+
+Sample Output :
+22
+
+*/
+
+//Working Solution :- 
+
+int solution(vector<int>arr){
+    int ans=INT_MIN;
+    int result=-1;
+    vector<int>weight(arr.size(),0);
+    for(int i=0;i<arr.size();i++){
+        int source=i;
+        int dest=arr[i];
+        if(dest!=-1){
+            weight[dest]+=source;
+            if(ans<=weight[dest]){
+                ans=max(ans,weight[dest]);
+                result=dest;
+            }
+            
+        }
+    }
+    if(ans!=INT_MIN)
+        return result;
+    return -1;
+}
