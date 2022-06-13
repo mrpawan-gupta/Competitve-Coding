c6a8638b939f64d2fb868f8440bcb12c2093eeef
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-13 08:41:53 +0530

    Template Updated

---
+++ ____Template2.cpp
@@ -0,0 +1,28 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin.failbit; cin.badbit;
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
