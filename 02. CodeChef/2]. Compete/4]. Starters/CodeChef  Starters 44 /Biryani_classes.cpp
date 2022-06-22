341239560395c80848de8088f962deeb6a71f311
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-22 23:37:02 +0530

    Solution Biryani classes

---
+++ 02. CodeChef/2]. Compete/4]. Starters/CodeChef  Starters 44 /Biryani_classes.cpp
@@ -0,0 +1,31 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void solve(){
+            int a , b;
+            cin >> a >> b;
+            long ans = a * b;
+            cout << ans << endl;
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin.failbit; cin.badbit;
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        Solution ob;
+        ob.solve();
+    }
+
+    return 0;
+}
