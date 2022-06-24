ac46a8d6b3eecc266e7bbd4ee25a2a07e84f41e6
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-24 15:12:28 +0530

    Solution 200. Number of Islands Added

--- 03. LeetCode/1]. Problems/C++/0200)_Number of Islands.cpp
+++ 03. LeetCode/1]. Problems/C++/0200)_Number of Islands.cpp
@@ -1,37 +1,25 @@
-class Solution
-{
+class Solution{
 public:
-    // FLOOD FILL ALGORITHM
-    // Two array to go in all four direction up,down,left,right
     int dx[4] = {-1, 0, 1, 0};
     int dy[4] = {0, 1, 0, -1};
-    void floodfill(vector<vector<char>> &grid, int i, int j)
-    {
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
-        {
+    void floodfill(vector<vector<char>> &grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
             return;
         }
-        if (grid[i][j] == '0')
-        {
+        if (grid[i][j] == '0') {
             return;
         }
         grid[i][j] = '0';
-        for (int x = 0; x < 4; x++)
-        {
+        for (int x = 0; x < 4; x++){
             floodfill(grid, i + dx[x], j + dy[x]);
         }
     }
-    int numIslands(vector<vector<char>> &grid)
-    {
+    int numIslands(vector<vector<char>> &grid)  {
         int count = 0;
-        for (int i = 0; i < grid.size(); i++)
-        {
-            for (int j = 0; j < grid[0].size(); j++)
-            {
-                if (grid[i][j] == '1')
-                {
-                    count++;               // number of time this function needs to be called is the ans
-                    floodfill(grid, i, j); // this function changes each index of a island to 0
+        for (int i = 0; i < grid.size(); ++i)  {
+            for (int j = 0; j < grid[0].size(); ++j){
+                if (grid[i][j] == '1'){
+                    count++;               
                 }
             }
         }
