7275d6775c69b2260362d66117da05a697eb7449
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-28 16:39:40 +0530

    GFG Solution : Wave Array

---
+++ 08. Geek4Geeks/Practice/Wave_Array.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    // code here
+	    int low = 0;
+	    int high = n-1;
+	    
+	    while(low < high) {
+	        int mid = (low+high) / 2;
+	        if(arr[mid] < arr[mid+1]) {
+	            low = mid +1;
+	        } else {
+	            high = mid;
+	        }
+	        
+	    }
+	    return arr[low];
+	}
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaximum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
