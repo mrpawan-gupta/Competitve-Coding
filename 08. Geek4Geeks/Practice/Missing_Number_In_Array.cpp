5b8d2db4a6e6d09be435fcd63586c821f8ccf1d0
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-28 16:40:42 +0530

    GFG Solution : Missing Number In Array

---
+++ 08. Geek4Geeks/Practice/Missing_Number_In_Array.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    // code here
+	    int low = 0;
+	    int high = n-1;
+	    
+	    while(low < high) {
+	        int mid = (low+high) / 2;
+	        if(arr[mid] < arr[mid+1]) {
+	            low = mid +1;
+	        } else {
+	            high = mid;
+	        }
+	        
+	    }
+	    return arr[low];
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaximum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
