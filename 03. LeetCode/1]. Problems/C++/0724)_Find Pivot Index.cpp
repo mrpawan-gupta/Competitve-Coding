db79adaa151412d786c0d50e42336619d3f79cd6
Pawan Gupta <guptapawanro2017@gmail.com>
2022-07-08 12:53:24 +0530

    Solution 724. Find Pivot Index Added

---
+++ 03. LeetCode/1]. Problems/C++/0724)_Find Pivot Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int rightSum = accumulate(nums.begin(),nums.end(),0);
+        int leftSum = 0;
+        //cout << rightSum << endl;
+        for(int i=0;i<nums.size();i++){            
+            rightSum -= nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
